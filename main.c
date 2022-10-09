8d247b05ccbaf57f22472d8cc112a7a1b8690fa4
Drogon-dev <69829036+Drogon-dev@users.noreply.github.com>
2022-10-09 17:05:54 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,97 +1,48 @@
 #include "monty.h"
 
-
-
 data_t data;
-
 stack_t *head_q = NULL;
-
 stack_t *bottom_s = NULL;
-
 int is_stack = 1;
 
-
-
 /**
+ * main - program entry point
+ * @argc: count of argv
+ * @argv: pointer to pointers pf command line arguments
  *
- *  * main - program entry point
- *
- *   * @argc: count of argv
- *
- *    * @argv: pointer to pointers pf command line arguments
- *
- *     *
- *
- *      * Return: EXIT_SUCCESS or EXIT_FAILURE
- *
- *       */
-
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
 int main(int argc, char *argv[])
-
 {
+	ssize_t line_read = 1;
+	size_t len = 0;
+	stack_t *stack = NULL;
 
-		ssize_t line_read = 1;
+	memset((void *) &data, 0, sizeof(data));
+	if (argc != 2)
+		get_error(1);
 
-			size_t len = 0;
+	data.filename = argv[1];
+	data.fp = fopen(data.filename, "r");
+	if (!data.fp)
+		get_error(2);
+	
+	while ((line_read = getline(&data.line, &len, data.fp)) > 0)
+	{
+		if (*data.line == '\n')
+			continue;
 
-				stack_t *stack = NULL;
+		data.line_number++;
+		free(data.args);
 
+		if (get_tokens() < 0)
+			continue;
+		if (*data.args == NULL)
+			continue;
+		get_op(&stack);
+	}
 
-
-					memset((void *) &data, 0, sizeof(data));
-
-						if (argc != 2)
-
-									get_error(1);
-
-
-
-							data.filename = argv[1];
-
-								data.fp = fopen(data.filename, "r");
-
-									if (!data.fp)
-
-												get_error(2);
-
-										
-
-										while ((line_read = getline(&data.line, &len, data.fp)) > 0)
-
-												{
-
-															if (*data.line == '\n')
-
-																			continue;
-
-
-
-																	data.line_number++;
-
-																			free(data.args);
-
-
-
-																					if (get_tokens() < 0)
-
-																									continue;
-
-																							if (*data.args == NULL)
-
-																											continue;
-
-																									get_op(&stack);
-
-																										}
-
-
-
-											free_data();
-
-												free_stack(stack);
-
-													return (EXIT_SUCCESS);
-
+	free_data();
+	free_stack(stack);
+	return (EXIT_SUCCESS);
 }
-
-
