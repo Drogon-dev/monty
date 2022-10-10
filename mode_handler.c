0ea5a95f40a6a6deb3cf7e00b78d43991ce53919
Drogon-dev <69829036+Drogon-dev@users.noreply.github.com>
2022-10-10 01:13:10 +0100

    Update mode_handler.c

---
+++ mode_handler.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+/**
+ * stack_mode - sets the format of the data to a stack (LIFO)
+ * @stack: double pointer to the first/last node depending on
+ * mode
+ * @line_number: line number
+ */
+void stack_mode(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+    if (!is_stack)
+    {
+        bottom_s = *stack;
+        *stack = head_q;
+        is_stack = 1;
+    }
+}
+
+/**
+ * queue_mode - sets the format of the data to a queue (FIFO)
+ * @stack: double pointer to the first/last node depending on
+ * mode
+ * @line_number: line number
+ */
+void queue_mode(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+    if (is_stack)
+    {
+        head_q = *stack;
+        *stack = bottom_s;
+        is_stack = 0;
+    }
+}
