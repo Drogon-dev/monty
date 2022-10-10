02cbe91d184aaf6d9bc5808222105fd68b33d900
Drogon-dev <69829036+Drogon-dev@users.noreply.github.com>
2022-10-10 02:40:45 +0100

    Update helpers_func.c

---
+++ helpers_func.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+int _isdigit(char *str);
+
+/**
+ * _isdigit - checks for a digit from 0 to 9
+ * @str: the integer to be checked
+ *
+ * Return: 1 if is digit, 0 otherwise
+ */
+int _isdigit(char *str)
+{
+        int i = 0;
+
+	if (str == NULL)
+		return (1);
+	while (str[i])
+	{
+		if (isdigit(*str) == 0)
+			return (1);
+		i++;
+	}
+	return (0);
+}
